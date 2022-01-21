956d4534a211ebcab46fd3e08f70d7cb34637e7f
alumno <alumno@Alumno14>
2022-01-21 13:20:56 +0100

    x2

--- proyecto/src/controller/Main.java
+++ proyecto/src/controller/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hola sebas :)");
+		System.out.println("hola sebas :) x2");
 	}
 
 }
